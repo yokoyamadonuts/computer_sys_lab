11cef8efa3ef48aad0f5d944c2ac75c70b41b4a5
yokoyamadonuts <yokoyama.shingo@donuts.ne.jp>
2025-05-06 18:43:23 +0900

    NOTゲートの実装

--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,27 @@ pub fn nand(a: bool, b: bool) -> bool {
     !(a && b)
 }
 
+/// NOT  (￢A) = NAND(A, A)
+pub fn not(a: bool) -> bool {
+    nand(a, a)
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn truth_table() {
+    fn nand_gate() {
         assert_eq!(nand(false, false), true);
         assert_eq!(nand(false, true),  true);
         assert_eq!(nand(true,  false), true);
         assert_eq!(nand(true,  true),  false);
+    }
+
+    #[test]
+    fn not_gate() {
+        assert_eq!(not(false), true);
+        assert_eq!(not(true),  false);
     }
 }
