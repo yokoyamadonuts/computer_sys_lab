037c8a90bf60ff4f300f30b1d4655234478283dc
yokoyamadonuts <yokoyama.shingo@donuts.ne.jp>
2025-05-06 18:31:52 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+/// 2入力 NAND ゲート
+///
+/// *真理値表*  
+/// A | B | NAND  
+/// 0 | 0 | 1  
+/// 0 | 1 | 1  
+/// 1 | 0 | 1  
+/// 1 | 1 | 0
+pub fn nand(a: bool, b: bool) -> bool {
+    !(a && b)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn truth_table() {
+        assert_eq!(nand(false, false), true);
+        assert_eq!(nand(false, true),  true);
+        assert_eq!(nand(true,  false), true);
+        assert_eq!(nand(true,  true),  false);
+    }
+}
